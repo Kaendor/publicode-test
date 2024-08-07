d4190cb31c9263e3bf797b987250fc40767b8e49
Dimitri Teboul <dimitri.teboul@indy.fr>
2024-08-07 10:57:33 +0200

    ✨ feat(domain): initial commit to test publicode for domain experts

---
+++ main.js
@@ -0,0 +1,12 @@
+import { getModelFromSource } from '@publicodes/tools/compilation';
+import Engine from "publicodes"
+
+// Publicode nécessite une phase de compilation du yaml vers JSON, faisable en amont de l'évaluation des règles, ici fait en même temps.
+export const parsedRules = () => {
+
+const rules = getModelFromSource('rules/test.yaml', {
+	verbose: true
+});
+
+return new Engine(rules);
+}
